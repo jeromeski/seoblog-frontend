009f9092b272ee0d290c0db4cfe03a565055e22d
jeromeski <jeromegee@gmail.com>
2020-09-23 11:28:46 +0800

    Layout page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import Layout from '../components/Layout';
+
 const Index = () => {
   return (
-    <div>
-      <h1>Index Page</h1>
-    </div>
+    <Layout>
+      <h2>Index page</h2>
+    </Layout>
   );
 };
 
