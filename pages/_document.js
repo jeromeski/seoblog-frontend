f8b8bd7e6d7fadc0418106488f63a15ea5a12592
jeromeski <jeromegee@gmail.com>
2020-09-24 18:27:44 +0800

    Using Nprogress

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,12 @@ class MyDocument extends Document {
             integrity='sha512-MoRNloxbStBcD8z3M/2BmnT+rg4IsMxPkXaGh2zD6LGNNFE80W3onsAhRcMAMrSoyWL9xD7Ert0men7vR8LUZg=='
             crossOrigin='anonymous'
           />
+          <link
+            rel='stylesheet'
+            href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
+            integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=='
+            crossOrigin='anonymous'
+          />
         </Head>
         <body>
           <Main />
