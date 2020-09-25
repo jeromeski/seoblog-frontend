679d88fb47a1c50665b1241753763c2e09955e67
jeromeski <jeromegee@gmail.com>
2020-09-25 15:44:02 +0800

    Category read delete and messages

--- components/crud/Category.js
+++ components/crud/Category.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Router from 'next/router';
 import { isAuth, getCookie } from '../../actions/auth';
-import { create } from '../../actions/category';
+import { create, getCategories, removeCategory } from '../../actions/category';
 
 const Category = () => {
   const [values, setValues] = useState({
@@ -10,10 +10,66 @@ const Category = () => {
     error: false,
     success: false,
     categories: [],
-    removed: false
+    removed: false,
+    reload: false
   });
-  const { name, error, success, categories, removed } = values;
+  const { name, error, success, categories, removed, reload } = values;
   const token = getCookie('token');
+
+  useEffect(() => {
+    loadCategories();
+  }, [reload]);
+
+  const loadCategories = () => {
+    getCategories().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setValues({ ...values, categories: data });
+      }
+    });
+  };
+
+  const deleteConfirm = slug => {
+    let answer = window.confirm(
+      'Are you sure you want to delete this category?'
+    );
+    if (answer) {
+      deleteCategory(slug);
+    }
+  };
+
+  const deleteCategory = slug => {
+    // console.log('delete', slug);
+    removeCategory(slug, token).then(data => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        setValues({
+          ...values,
+          error: false,
+          success: false,
+          name: '',
+          removed: !removed,
+          reload: !reload
+        });
+      }
+    });
+  };
+
+  const showCategories = () => {
+    return categories.map((c, i) => {
+      return (
+        <button
+          title='Double click to delete'
+          key={i}
+          className='btn btn-outline-primary mr-1 ml-1 mt-3'
+          onDoubleClick={() => deleteConfirm(c.slug)}>
+          {c.name}
+        </button>
+      );
+    });
+  };
 
   const clickSubmit = e => {
     e.preventDefault();
@@ -22,7 +78,14 @@ const Category = () => {
       if (data.error) {
         setValues({ ...values, error: data.error, success: false });
       } else {
-        setValues({ ...values, error: false, success: true, name: '' });
+        setValues({
+          ...values,
+          error: false,
+          success: false,
+          name: '',
+          removed: !removed,
+          reload: !reload
+        });
       }
     });
   };
@@ -37,12 +100,37 @@ const Category = () => {
     });
   };
 
+  const showSuccess = () => {
+    if (success) {
+      return <p className='text-success'>Category is created</p>;
+    }
+  };
+  const showError = () => {
+    if (error) {
+      return <p className='text-danger'>Category is created</p>;
+    }
+  };
+  const showRemoved = () => {
+    if (removed) {
+      return <p className='text-danger'>Category is removed</p>;
+    }
+  };
+
+  const mouseMoveHandler = e => {
+    setValues({ ...values, error: false, success: false, removed: '' });
+  };
+
   const newCategoryForm = () => {
     return (
       <form onSubmit={clickSubmit}>
         <div className='form-group'>
           <label className='text-muted'>Name</label>
-          <input className='form-control' onChange={handleChange} />
+          <input
+            className='form-control'
+            onChange={handleChange}
+            value={name}
+            required
+          />
         </div>
         <div>
           <button type='submit' className='btn btn-primary'>
@@ -53,7 +141,18 @@ const Category = () => {
     );
   };
 
-  return <React.Fragment>{newCategoryForm()}</React.Fragment>;
+  return (
+    <React.Fragment>
+      {showSuccess()}
+      {showError()}
+      {showRemoved()}
+
+      <div onMouseMove={mouseMoveHandler}>
+        {newCategoryForm()}
+        {showCategories()}
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Category;
