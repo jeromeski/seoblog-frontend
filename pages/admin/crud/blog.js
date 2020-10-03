f17a567059b3da604ed55ff87845e7f9a8e71c40
jeromeski <jeromegee@gmail.com>
2020-10-03 17:35:17 +0800

    Update blog - Populate content in rich text editor

--- pages/admin/crud/blog.js
+++ pages/admin/crud/blog.js
@@ -4,22 +4,22 @@ import BlogCreate from '../../../components/crud/BlogCreate';
 import Link from 'next/link';
 
 const Blog = () => {
-  return (
-    <Layout>
-      <Admin>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-md-12 pt-5 pb-5'>
-              <h2>Create a new blog</h2>
-            </div>
-            <div className='col-md-12'>
-              <BlogCreate />
-            </div>
-          </div>
-        </div>
-      </Admin>
-    </Layout>
-  );
+    return (
+        <Layout>
+            <Admin>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-12 pt-5 pb-5">
+                            <h2>Create a new blog</h2>
+                        </div>
+                        <div className="col-md-12">
+                            <BlogCreate />
+                        </div>
+                    </div>
+                </div>
+            </Admin>
+        </Layout>
+    );
 };
 
 export default Blog;
