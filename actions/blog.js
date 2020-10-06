05232e2542e5f806b0d6cd445f0b7dbcfcf320b6
jeromeski <jeromegee@gmail.com>
2020-10-06 15:37:42 +0800

    Auth user can update delete blog fronten

--- actions/blog.js
+++ actions/blog.js
@@ -4,121 +4,145 @@ import queryString from 'query-string';
 import { isAuth } from './auth';
 
 export const createBlog = (blog, token) => {
-  let createdBlogEndPoint;
+    let createBlogEndpoint;
 
-  if (isAuth() && isAuth().role === 1) {
-    createdBlogEndPoint = `{API}/blog`;
-  } else if (isAuth() && isAuth().role === 0) {
-    createdBlogEndPoint = `${API}/user/blog`;
-  }
+    if (isAuth() && isAuth().role === 1) {
+        createBlogEndpoint = `${API}/blog`;
+    } else if (isAuth() && isAuth().role === 0) {
+        createBlogEndpoint = `${API}/user/blog`;
+    }
 
-  return fetch(`${createdBlogEndPoint}`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      Authorization: `Bearer ${token}`
-    },
-    body: blog
-  })
-    .then(response => {
-      return response.json();
+    return fetch(`${createBlogEndpoint}`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: blog
     })
-    .catch(err => console.log(err));
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
 };
 
 export const listBlogsWithCategoriesAndTags = (skip, limit) => {
-  const data = {
-    limit,
-    skip
-  };
-  return fetch(`${API}/blogs-categories-tags`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  })
-    .then(response => {
-      return response.json();
+    const data = {
+        limit,
+        skip
+    };
+    return fetch(`${API}/blogs-categories-tags`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
     })
-    .catch(err => console.log(err));
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
 };
 
 export const singleBlog = slug => {
-  return fetch(`${API}/blog/${slug}`, {
-    method: 'GET'
-  })
-    .then(response => {
-      return response.json();
+    return fetch(`${API}/blog/${slug}`, {
+        method: 'GET'
     })
-    .catch(err => console.log(err));
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
 };
 
 export const listRelated = blog => {
-  return fetch(`${API}/blogs/related`, {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(blog)
-  })
-    .then(response => {
-      return response.json();
+    return fetch(`${API}/blogs/related`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(blog)
     })
-    .catch(err => console.log(err));
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
 };
 
-export const list = () => {
-  return fetch(`${API}/blogs`, {
-    method: 'GET'
-  })
-    .then(response => {
-      return response.json();
+export const list = username => {
+    let listBlogsEndpoint;
+
+    if (username) {
+        listBlogsEndpoint = `${API}/${username}/blogs`;
+    } else {
+        listBlogsEndpoint = `${API}/blogs`;
+    }
+
+    return fetch(`${listBlogsEndpoint}`, {
+        method: 'GET'
     })
-    .catch(err => console.log(err));
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
 };
 
 export const removeBlog = (slug, token) => {
-  return fetch(`${API}/blog/${slug}`, {
-    method: 'DELETE',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`
+    let deleteBlogEndpoint;
+
+    if (isAuth() && isAuth().role === 1) {
+        deleteBlogEndpoint = `${API}/blog/${slug}`;
+    } else if (isAuth() && isAuth().role === 0) {
+        deleteBlogEndpoint = `${API}/user/blog/${slug}`;
     }
-  })
-    .then(response => {
-      return response.json();
+
+    return fetch(`${deleteBlogEndpoint}`, {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`
+        }
     })
-    .catch(err => console.log(err));
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
 };
 
 export const updateBlog = (blog, token, slug) => {
-  return fetch(`${API}/blog/${slug}`, {
-    method: 'PUT',
-    headers: {
-      Accept: 'application/json',
-      Authorization: `Bearer ${token}`
-    },
-    body: blog
-  })
-    .then(response => {
-      return response.json();
+    let updateBlogEndpoint;
+
+    if (isAuth() && isAuth().role === 1) {
+        updateBlogEndpoint = `${API}/blog/${slug}`;
+    } else if (isAuth() && isAuth().role === 0) {
+        updateBlogEndpoint = `${API}/user/blog/${slug}`;
+    }
+
+    return fetch(`${updateBlogEndpoint}`, {
+        method: 'PUT',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: blog
     })
-    .catch(err => console.log(err));
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
 };
 
 export const listSearch = params => {
-  console.log('search params', params);
-  let query = queryString.stringify(params);
-  console.log('query params', query);
-  return fetch(`${API}/blogs/search?${query}`, {
-    method: 'GET'
-  })
-    .then(response => {
-      return response.json();
+    console.log('search params', params);
+    let query = queryString.stringify(params);
+    console.log('query params', query);
+    return fetch(`${API}/blogs/search?${query}`, {
+        method: 'GET'
     })
-    .catch(err => console.log(err));
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
 };
